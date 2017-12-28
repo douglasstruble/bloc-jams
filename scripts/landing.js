65c460801b0f250067f79b7dbee3db5da1a74a50
douglasstruble <dastruble@gmail.com>
2017-12-28 13:48:28 -0800

    DOM22_notworking

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,31 +1,25 @@
  var pointsArray = document.getElementsByClassName('point');
  
- var animatePoints = function(points) {
+      var revealFirstPoint = function(point) {
+         points.style.opacity = 1;
+         points.style.transform = "scaleX(1) translateY(0)";
+         points.style.msTransform = "scaleX(1) translateY(0)";
+         points.style.WebkitTransform = "scaleX(1) translateY(0)";
+     };
+
+     var animatePoints = function(points) {
+         forEach(points, revealPoint);
  
-     var revealFirstPoint = function(index) {
-         points[index].style.opacity = 1;
-         points[index].style.transform = "scaleX(1) translateY(0)";
-         points[index].style.msTransform = "scaleX(1) translateY(0)";
-         points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-     }
- 
-     for (var i = 0; i < points.length; i++) {
-         revealPoint(i);   
-     }
  };
 
- window.onload = function() {
-     
+ window.onload = function() {   
      if (window.innerHeight > 950) {
          animatePoints(pointsArray);
      }
-       var sellingPoints = document.getElementsByClassName('selling-points')[0];
-   var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-     
-     
+       
     window.addEventListener('scroll', function(event) {
-if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-             animatePoints(pointsArray);   
+if (pointsArray[0].getBoundingClientRect().top <=500) {
+            animatePoints(pointsArray);   
          }
      });
  }
