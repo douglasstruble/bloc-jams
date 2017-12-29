4a8219f74dfbe5d92e387a258a458a5bfd7170fc
douglasstruble <dastruble@gmail.com>
2017-12-29 12:28:10 -0800

    28JQuery

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,26 +1,34 @@
- var pointsArray = document.getElementsByClassName('point');
- 
-      var revealFirstPoint = function(point) {
-         points.style.opacity = 1;
-         points.style.transform = "scaleX(1) translateY(0)";
-         points.style.msTransform = "scaleX(1) translateY(0)";
-         points.style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
+ // #5
+ var animatePoints = function() {
 
-     var animatePoints = function(points) {
-         forEach(points, revealPoint);
- 
+     var revealPoint = function() {
+         // #7
+         $(this).css({
+             opacity: 1,
+             transform: 'scaleX(1) translateY(0)'
+         });
+
+     };
+// #6
+     $.each($('.point'), revealPoint);
  };
 
- window.onload = function() {   
-     if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
+ $(window).load(function() {
+// #1
+     if ($(window).height() > 950) {
+         animatePoints();
      }
+     
+// #2
+     var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
        
-    window.addEventListener('scroll', function(event) {
-if (pointsArray[0].getBoundingClientRect().top <=500) {
-            animatePoints(pointsArray);   
+// #3
+     $(window).scroll(function(event) {
+
+// #4
+         if ($(window).scrollTop() >= scrollDistance) {
+             animatePoints();
+           
          }
      });
- }
-    
+});    
