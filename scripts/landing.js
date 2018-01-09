c9db211ac9d761a87e545a2d50f52db642803601
douglasstruble <dastruble@gmail.com>
2018-01-08 16:57:24 -0800

    DOMSevents01.08.18

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,34 +1,25 @@
- // #5
- var animatePoints = function() {
+ var pointsArray = document.getElementsByClassName('point');
 
-     var revealPoint = function() {
-         // #7
-         $(this).css({
-             opacity: 1,
-             transform: 'scaleX(1) translateY(0)'
-         });
-
+     var revealPoint = function(point) {
+         point.style.opacity =1;
+         point.style.transform = "scaleX(1) translateY(0)";
+         point.style.msTransform = "scaleX(1) translateY(0)";
+         point.style.WebkitTransform = "scaleX(1) translateY(0)";
      };
-// #6
-     $.each($('.point'), revealPoint);
+     
+var animatePoints = function(points) {
+        forEach(points, revealPoint);
  };
 
- $(window).load(function() {
-// #1
-     if ($(window).height() > 950) {
-         animatePoints();
+
+ $(window).onload(function() {
+     if ($(window).innerHeight > 950) {
+         animatePoints(pointsArray);
      }
      
-// #2
-     var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
-       
-// #3
-     $(window).scroll(function(event) {
-
-// #4
-         if ($(window).scrollTop() >= scrollDistance) {
-             animatePoints();
-           
+     (window).addEventListener("scroll", function(event) {
+         if (pointsArray(0).getBoundingClientRect().top <=500)  {
+            
          }
      });
-});    
+}    
